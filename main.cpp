c6db14b8870867320de8424b0e4d39796d2764d7
Darrell Wright <beached@users.noreply.github.com>
2020-11-12 14:24:32 -0500

    .

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@
 
 int main( ) {
 	auto srv = daw::json_rpc::json_rpc_dispatch(
-	  "add", daw::json_rpc::request_handler<int( int, int )>(
-	           []( int l, int r ) { return l + r; } ) );
+	  "mul", daw::json_rpc::request_handler<int( int, int )>(
+	           []( int l, int r ) { return l * r; } ) );
 	constexpr std::string_view json_req =
-	  R"({ "jsonrpc": "2.0", "method": "add", "params": [1,2] })";
+	  R"({ "jsonrpc": "2.0", "method": "mul", "params": [45,22] })";
 	std::cout << "req: " << json_req << '\n';
 	auto resp = std::string( 1024, '\0' );
 
-	srv( "add", json_req, { }, resp.data( ) );
+	srv( "mul", json_req, { }, resp.data( ) );
 	std::cout << "resp: " << resp << '\n';
 
 	return 0;
