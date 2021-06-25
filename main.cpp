7d5b7aad2b0b67f95f8508fe1098190ca5b1f8d2
Darrell Wright <beached@users.noreply.github.com>
2021-06-25 02:43:57 -0400

    cleaned up name

--- main.cpp
+++ main.cpp
@@ -31,17 +31,17 @@ namespace daw::json {
 } // namespace daw::json
 
 int main( ) {
-	auto server = daw::json_rpc::json_rpc_server( );
-	auto disp = daw::json_rpc::json_rpc_dispatch( );
-	disp.add_method( "mul", []( int l, int r ) { return l * r; } );
-	disp.add_method( "add", add );
 	// { "jsonrpc": "2.0", "method": "mul", "params": [45,22] }
-	disp.add_method( "status", []( Foo f ) {
+	auto server = daw::json_rpc::json_rpc_server( );
+	auto dispatcher = daw::json_rpc::json_rpc_dispatch( );
+	dispatcher.add_method( "mul", []( int l, int r ) { return l * r; } );
+	dispatcher.add_method( "add", add );
+	dispatcher.add_method( "status", []( Foo f ) {
 		std::cout << "Hello" << f.bar << '\n';
 		return f.d;
 	} );
 
-	server.add_dispatcher( "/rpc", disp );
+	server.add_dispatcher( "/rpc", dispatcher );
 	server.listen( "localhost", 1234 );
 	return 0;
 }
